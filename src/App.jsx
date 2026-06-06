97411aef68e5f9faa01a085cf9cf923bd6a1f3d9
jk-jumur <developer.jumur@gmail.com>
2026-06-07 04:13:41 +0600

    access form data using unsubmit and E target

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+         <div>
+             <h2>Explore react form</h2>
+         </div>
+  
+    </>
+  )
+}
+
+export default App
